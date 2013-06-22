920c033debe061ad06c5fdbeb77693a071101052
David Graham <david.malcom.graham@gmail.com>
2013-06-22 15:51:43 -0700

    Add command classes.

---
+++ lib/vines/web/command/init.rb
@@ -0,0 +1,31 @@
+module Vines
+  module Web
+    module Command
+      class Init
+        def run(opts)
+          raise 'vines-web init <domain>' unless opts[:args].size == 1
+          raise "vines gem required: gem install vines" unless vines_installed?
+
+          domain = opts[:args].first.downcase
+          base = File.expand_path(domain)
+
+          `vines init #{domain}` unless File.exists?(base)
+
+          web = File.expand_path("../../../../../public", __FILE__)
+          FileUtils.cp_r(Dir.glob("#{web}/*"), 'web')
+          puts "Web assets installed: #{domain}"
+
+          `type open && open http://localhost:5280/`
+        end
+
+        private
+
+        def vines_installed?
+          require 'vines/version'
+        rescue LoadError
+          false
+        end
+      end
+    end
+  end
+end
---
+++ lib/vines/web/command/install.rb
@@ -0,0 +1,18 @@
+module Vines
+  module Web
+    module Command
+      class Install
+        def run(opts)
+          raise 'vines-web install <dir>' unless opts[:args].size == 1
+
+          dir = opts[:args].first.downcase
+          dir = File.expand_path(dir)
+
+          web = File.expand_path("../../../../../public", __FILE__)
+          FileUtils.cp_r(Dir.glob("#{web}/*"), dir)
+          puts "Web assets installed: #{dir}"
+        end
+      end
+    end
+  end
+end
