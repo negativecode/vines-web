33c7235e46fe9c4b2e351f79c03cb9a2f8728b9c
David Graham <david.malcom.graham@gmail.com>
2013-06-23 09:45:52 -0700

    Add paths method for asset reuse.

--- lib/vines/web.rb
+++ lib/vines/web.rb
@@ -5,9 +5,18 @@ require 'vines/web/command/install'
 
 module Vines
   module Web
+    # Return javascript and stylesheet asset paths to be added to a sprockets
+    # environment. This allows the lib and vendor javascript bundles to be
+    # shared by other apps.
+    #
+    #   env = Sprockets::Environment.new
+    #   Vines::Web.paths.each {|path| env.append_path path }
+    #
+    # Returns an Array of String paths.
     def self.paths
-      js  = File.expand_path('../../../app/assets/javascripts', __FILE__)
-      css = File.expand_path('../../../app/assets/stylesheets', __FILE__)
+      root = File.expand_path('../../..', __FILE__)
+      js = '%s/app/assets/javascripts' % root
+      css = '%s/app/assets/stylesheets' % root
       [js, css]
     end
   end
