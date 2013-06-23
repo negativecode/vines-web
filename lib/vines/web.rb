0f5e4ab3d938030c675ae4b86a4ed0f68daead5e
David Graham <david.malcom.graham@gmail.com>
2013-06-22 20:32:05 -0700

    Add paths method for assets.

--- lib/vines/web.rb
+++ lib/vines/web.rb
@@ -2,3 +2,13 @@ require 'fileutils'
 require 'vines/web/version'
 require 'vines/web/command/init'
 require 'vines/web/command/install'
+
+module Vines
+  module Web
+    def self.paths
+      js  = File.expand_path('../../../app/assets/javascripts', __FILE__)
+      css = File.expand_path('../../../app/assets/stylesheets', __FILE__)
+      [js, css]
+    end
+  end
+end
